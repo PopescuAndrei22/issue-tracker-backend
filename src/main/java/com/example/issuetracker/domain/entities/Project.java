b57109ea659daf733023503ac813a57b3ae9383c
PopescuAndrei22 <andrei.popescu2708@gmail.com>
2026-02-07 15:59:53 +0200

    fix(domain): Fix a bug where the User entity doesn't appear in the database as a table and add the inverse relationships for navigation to the entities

--- src/main/java/com/example/issuetracker/domain/entities/Project.java
+++ src/main/java/com/example/issuetracker/domain/entities/Project.java
@@ -1,18 +1,27 @@
 package com.example.issuetracker.domain.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name  = "projects")
 public class Project extends BaseEntity{
 
     @Column(nullable = false, length = 100)
     private String name;
 
     private String description;
+
+    @OneToMany(mappedBy="project")
+    private List<Issue> issues = new ArrayList<>();
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "owner_id", nullable = false)
--- src/main/java/com/example/issuetracker/domain/entities/User.java
+++ src/main/java/com/example/issuetracker/domain/entities/User.java
@@ -1,13 +1,19 @@
 package com.example.issuetracker.domain.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.issuetracker.domain.models.Role;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name  = "users")
 public class User extends BaseEntity {
 
     @Column(nullable = false, length = 50)
@@ -22,4 +28,13 @@ public class User extends BaseEntity {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
+
+    @OneToMany(mappedBy="owner")
+    private List<Project> projects = new ArrayList<>();
+
+    @OneToMany(mappedBy="assignee")
+    private List<Issue> assignedIssues = new ArrayList<>();
+
+    @OneToMany(mappedBy="reporter")
+    private List<Issue> reportedIssues = new ArrayList<>();
 }
