eb5d46168b62fa7752bb55a09a80d1fee252a091
PopescuAndrei22 <andrei.popescu2708@gmail.com>
2026-02-11 19:44:14 +0200

    test(Issue): Add test for building an Issue object

--- src/test/java/com/example/issuetracker/domain/IssueTest.java
+++ src/test/java/com/example/issuetracker/domain/IssueTest.java
@@ -1,12 +1,31 @@
 package com.example.issuetracker.domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+
+import com.example.issuetracker.domain.entities.Issue;
+import com.example.issuetracker.domain.entities.Project;
+import com.example.issuetracker.domain.entities.User;
+import com.example.issuetracker.domain.models.IssueType;
+import com.example.issuetracker.domain.models.Priority;
 
 public class IssueTest {
 
     @Test
-    public void test(){
-        fail();
+    public void shouldThrowErrorWhenTitleIsMissing(){
+
+        User mockReporter = mock(User.class);
+        Project mockProject = mock(Project.class);
+
+        assertThatThrownBy(() -> 
+                Issue.builder()
+                    .reporter(mockReporter)
+                    .project(mockProject)
+                    .priority(Priority.MEDIUM)
+                    .type(IssueType.BUG)
+                    .build())
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
