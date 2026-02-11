76f843a00b5e55be2c2ef5e398897d97fa322255
PopescuAndrei22 <andrei.popescu2708@gmail.com>
2026-02-11 19:25:45 +0200

    refactor(test): Removed the initial Test file provided in the project template

---
+++ src/test/java/com/example/issuetracker/domain/IssueTest.java
@@ -0,0 +1,12 @@
+package com.example.issuetracker.domain;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+
+public class IssueTest {
+
+    @Test
+    public void test(){
+        fail();
+    }
+}
