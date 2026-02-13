ab6e14936803936bd5675753b617b5b937fa6c8d
PopescuAndrei22 <andrei.popescu2708@gmail.com>
2026-02-13 14:01:23 +0200

    test(domain): Add more tests for Issue to check if it is in a valid state after instantiation

--- src/test/java/com/example/issuetracker/domain/IssueTest.java
+++ src/test/java/com/example/issuetracker/domain/IssueTest.java
@@ -2,6 +2,8 @@ package com.example.issuetracker.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.mock;
 
@@ -10,21 +12,72 @@ import com.example.issuetracker.domain.entities.Project;
 import com.example.issuetracker.domain.entities.User;
 import com.example.issuetracker.domain.models.IssueType;
 import com.example.issuetracker.domain.models.Priority;
+import com.example.issuetracker.domain.models.Role;
 
 public class IssueTest {
+
+    static User reporter;
+    static Project project;
+
+    @BeforeAll
+    static void initialSetUp(){
+        reporter = mock(User.class);
+        project = mock(Project.class);
+    }
 
     @Test
     public void shouldThrowErrorWhenTitleIsMissing(){
 
-        User mockReporter = mock(User.class);
-        Project mockProject = mock(Project.class);
+        assertThatThrownBy(() -> 
+                Issue.builder()
+                    .reporter(reporter)
+                    .project(project)
+                    .type(IssueType.BUG)
+                    .build())
+            .isInstanceOf(IllegalArgumentException.class);
 
         assertThatThrownBy(() -> 
                 Issue.builder()
-                    .reporter(mockReporter)
-                    .project(mockProject)
-                    .priority(Priority.MEDIUM)
+                    .title("")
+                    .reporter(reporter)
+                    .project(project)
                     .type(IssueType.BUG)
+                    .build())
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void shouldThrowErrorWhenReporterIsMissing(){
+
+        assertThatThrownBy(() -> 
+                Issue.builder()
+                    .title("Fix bug login")
+                    .project(project)
+                    .type(IssueType.BUG)
+                    .build())
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void shouldThrowErrorWhenProjectIsMissing(){
+
+        assertThatThrownBy(() -> 
+                Issue.builder()
+                    .title("Fix bug login")
+                    .reporter(reporter)
+                    .type(IssueType.BUG)
+                    .build())
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void shouldThrowErrorWhenIssueTypeIsMissing(){
+
+        assertThatThrownBy(() -> 
+                Issue.builder()
+                    .title("Fix bug login")
+                    .reporter(reporter)
+                    .project(project)
                     .build())
             .isInstanceOf(IllegalArgumentException.class);
     }
