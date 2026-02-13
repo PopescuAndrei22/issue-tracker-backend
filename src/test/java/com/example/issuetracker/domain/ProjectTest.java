0ca06fc9ab4138ecb697c75415a2ed811d78833d
PopescuAndrei22 <andrei.popescu2708@gmail.com>
2026-02-13 13:27:46 +0200

    test(domain): Add tests for Project class to check if it is in a valid state after instantiation

---
+++ src/test/java/com/example/issuetracker/domain/ProjectTest.java
@@ -0,0 +1,47 @@
+package com.example.issuetracker.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.example.issuetracker.domain.entities.Project;
+import com.example.issuetracker.domain.entities.User;
+import com.example.issuetracker.domain.models.Role;
+
+public class ProjectTest {
+
+    static User owner;
+    
+    @BeforeAll
+    static void initialSetUp(){
+        String email = "Andrew@mycompany.com";
+        String username = "andrew123";
+        String password = "somepassword";
+        Role role = Role.USER;
+
+        owner = new User(username, password, email, role);
+    }
+
+    @Test
+    void shouldThrowErrorWhenProjectNameIsMissing(){
+        assertThatThrownBy(() -> 
+                new Project("", owner, "project description")
+            )
+            .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> 
+                new Project(null, owner, "project description")
+            )
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void shouldThrowErrorWhenProjectOwnerIsMissing(){
+        assertThatThrownBy(() -> 
+                new Project("project name", null, "project description")
+            )
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
