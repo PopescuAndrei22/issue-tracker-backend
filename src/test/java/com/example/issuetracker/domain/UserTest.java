b7e101ac59758d6ce5e5941caeabd95f81f3bf55
PopescuAndrei22 <andrei.popescu2708@gmail.com>
2026-02-13 12:53:20 +0200

    test(domain): Add test for User class

---
+++ src/test/java/com/example/issuetracker/domain/UserTest.java
@@ -0,0 +1,33 @@
+package com.example.issuetracker.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.example.issuetracker.domain.entities.Issue;
+import com.example.issuetracker.domain.entities.User;
+import com.example.issuetracker.domain.models.IssueType;
+import com.example.issuetracker.domain.models.Role;
+
+public class UserTest {
+
+    static User user;
+
+    @BeforeAll
+    static void initialSetUp(){
+        String email = "Andrew@mycompany.com";
+        String username = "andrew123";
+        String password = "somepassword";
+        Role role = Role.USER;
+
+        user = new User(username, password, email, role);
+    }
+
+    @Test
+    void shouldThrowErrorWhenUsernameIsMissing(){
+        assertThatThrownBy(() -> 
+                new User(null, "password", "email@company.com", Role.ADMIN))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
