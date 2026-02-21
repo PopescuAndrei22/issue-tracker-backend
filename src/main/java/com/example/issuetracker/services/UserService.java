63b017451bb735e0604a3a1ba72c2967622f1373
PopescuAndrei22 <andrei.popescu2708@gmail.com>
2026-02-21 20:59:42 +0200

    feat(services): Add the remaining dependencies for the UserService

--- src/main/java/com/example/issuetracker/services/UserService.java
+++ src/main/java/com/example/issuetracker/services/UserService.java
@@ -1,17 +1,28 @@
 package com.example.issuetracker.services;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.issuetracker.repositories.UserRepository;
+import com.example.issuetracker.web.mappers.UserMapper;
 
 import jakarta.transaction.Transactional;
 
 @Service
 @Transactional
 public class UserService {
-    UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserMapper userMapper;
 
-    public UserService(UserRepository userRepository){
+    public UserService
+    (
+        UserRepository userRepository,
+        PasswordEncoder passwordEncoder,
+        UserMapper userMapper
+    ){
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.userMapper = userMapper;
     }
 }
