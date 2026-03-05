24d398c3113282d94057569ef9079cf7fdb22e54
PopescuAndrei22 <andrei.popescu2708@gmail.com>
2026-03-05 16:18:28 +0200

    test(services): Test that the ProjectService creates the project successfully

--- src/test/java/com/example/issuetracker/services/ProjectServiceTest.java
+++ src/test/java/com/example/issuetracker/services/ProjectServiceTest.java
@@ -1,5 +1,77 @@
 package com.example.issuetracker.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.issuetracker.domain.entities.Project;
+import com.example.issuetracker.domain.entities.User;
+import com.example.issuetracker.domain.models.Role;
+import com.example.issuetracker.repositories.ProjectRepository;
+import com.example.issuetracker.web.dto.ProjectCreateDTO;
+import com.example.issuetracker.web.mappers.ProjectMapper;
+
+@ExtendWith(MockitoExtension.class)
 class ProjectServiceTest {
 
+    @Mock
+    private ProjectRepository projectRepository;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private ProjectMapper projectMapper;
+
+    @InjectMocks
+    private ProjectService projectService;
+
+    private User owner;
+
+    @BeforeEach
+    void setupOwner(){
+        String username = "Andrew22";
+        String password = "simplepassword";
+        String email = "andrew@email.com";
+        Role role = Role.ADMIN;
+        owner = new User(username, password, email, role);
+    }
+
+    @Test
+    void shouldCreateTheProjectSuccessfullyWithExistingOwner(){
+        String projectName = "firstproject";
+        Long ownerId = 1L;
+        String description = "It does a lot of things";
+        ProjectCreateDTO dto = new ProjectCreateDTO(
+            projectName,
+            ownerId,
+            description
+        );
+        when(projectRepository.save(any(Project.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        when(userService.getUserEntityById(ownerId)).thenReturn(owner);
+
+        projectService.createProject(dto);
+
+        verify(userService).getUserEntityById(ownerId);
+        ArgumentCaptor<Project> projectCaptor = ArgumentCaptor.forClass(Project.class);
+        verify(projectRepository).save(projectCaptor.capture());
+        Project project = projectCaptor.getValue();
+        assertEquals(project.getOwner(), owner);
+        assertEquals(project.getName(), projectName);
+        assertEquals(project.getDescription(), description);
+        verify(projectMapper).toResponseDTO(any(Project.class));
+    }
 }
