56e14d5024cfa2326ea12730433c7d449ff4ec70
PopescuAndrei22 <andrei.popescu2708@gmail.com>
2026-04-16 18:45:05 +0300

    feat(repositories): Add methods to fetch Projects and Issues based on the given user ID. Use @EntityGraph to avoid the N+1 problem

--- src/main/java/com/example/issuetracker/repositories/IssueRepository.java
+++ src/main/java/com/example/issuetracker/repositories/IssueRepository.java
@@ -1,9 +1,20 @@
 package com.example.issuetracker.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.issuetracker.domain.entities.Issue;
 
 public interface IssueRepository extends JpaRepository<Issue, Long>{
 
+    @EntityGraph(attributePaths = {"project", "reporter", "assignee"})
+    Page<Issue> findByAssigneeId(Long assigneeId, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"project", "reporter", "assignee"})
+    Page<Issue> findByReporterId(Long reporterId, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"project", "reporter", "assignee"})
+    Page<Issue> findByProjectId(Long projectId, Pageable pageable);
 }
--- src/main/java/com/example/issuetracker/repositories/ProjectRepository.java
+++ src/main/java/com/example/issuetracker/repositories/ProjectRepository.java
@@ -1,9 +1,14 @@
 package com.example.issuetracker.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.issuetracker.domain.entities.Project;
 
 public interface ProjectRepository extends JpaRepository<Project, Long>{
-
+    
+    @EntityGraph(attributePaths = {"owner"})
+    Page<Project> findByOwnerId(Long ownerId, Pageable pageable);
 }
